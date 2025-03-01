0d9c312954aa07f95c4db6edfd368a397c22d02c
Tanjilur Rahman <trmtanjil02@gmail.com>
2025-03-01 12:12:42 +0600

    button click  nev numbe number pluss  commit

--- script.js
+++ script.js
@@ -5,12 +5,18 @@ for (let i=0; i<completebutton.length; i++){
     completitem =   cmpltbtn.addEventListener('click',function(value){
    alert('Board Updated Succenfull')
  const tasknmbr=  document.getElementById('task-nmbr');
- const valuee = value.target.innerText ==='' ?1 : -1;
- console.log(valuee);
+ const valuee = value.target.innerText ==='' ? 1 : -1;
  const taskNmbeInt =parseInt(tasknmbr.innerText);
  const newtastNumber = Math.max(0, taskNmbeInt + valuee);
  tasknmbr.innerText = newtastNumber;
 
-  
-   })
+//   nev number pluss add 
+const nevNumber = document.getElementById('nev-number')
+        const nevNumPlus = value.target.innerText==='' ?-1: 1;
+        const nevNumInt = parseInt(nevNumber.innerText);
+        const newNaveNumber = Math.max(0, nevNumInt+nevNumPlus);
+        nevNumber.innerText= newNaveNumber;
+
+
+    })
 }
