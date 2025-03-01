ca30cff3ae330617023aae8284a50b20645ba4d3
Tanjilur Rahman <trmtanjil02@gmail.com>
2025-03-01 16:45:02 +0600

    card title time commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const completebutton = document.querySelectorAll('.completed-btn');
+
 for (let i=0; i<completebutton.length; i++){
-    cmpltbtn= completebutton[i];
+  const  cmpltbtn= completebutton[i];
  
     completitem =   cmpltbtn.addEventListener('click',function(value){
    alert('Board Updated Succenfull')
@@ -17,6 +18,28 @@ const nevNumber = document.getElementById('nev-number')
         const newNaveNumber = Math.max(0, nevNumInt+nevNumPlus);
         nevNumber.innerText= newNaveNumber;
 
+        // card messege import section start 
 
+    const cardDiv =document.getElementById('card-div');
+    const todayDate = new Date();
+    const hour= todayDate.getHours()||12;
+    const minut = todayDate.getMinutes();
+    const socends = todayDate.getSeconds();
+    const tomeZon = todayDate.getHours()>=12 ? "PM": "AM";
+    const newtTime = `
+    ${hour}: ${minut}:${socends}:${tomeZon}
+    `
+
+    const div = document.createElement("div");
+    
+    div.classList.add('bg-[#c8d6e5]')
+    div.innerHTML=`
+    <h1 class="text-black text-center">tanjil
+    ${newtTime}
+    </h1>
+    `
+     
+    cardDiv.appendChild(div)
     })
+ 
 }
